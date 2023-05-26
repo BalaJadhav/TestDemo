9a786e105925dd6eba88521cbe21c6f22a7ffdcd
BalaJadhav <balujadhav3938@gamil.com>
2023-05-26 17:32:11 +0530

    addition code is added

---
+++ TestDemo/src/Example.java
@@ -0,0 +1,15 @@
+
+public class Example {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		
+		System.out.println("Addition >>"+c);
+		System.out.println("Hello World....");
+		
+	}
+
+}
